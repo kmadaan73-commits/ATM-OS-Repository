9d43e5896f255c15ca55ac988dc767ed0dc895d9
kmadaan73-commits <kmadaan73@gmail.com>
2026-07-27 16:03:36 +0530

    feat(identity): create users table

--- database/users.sql
+++ database/users.sql
@@ -1,1 +1,25 @@
 
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+
+    email VARCHAR(255) UNIQUE NOT NULL,
+    mobile VARCHAR(20),
+
+    password_hash TEXT NOT NULL,
+
+    company_id BIGINT,
+
+    status ENUM(
+        'ACTIVE',
+        'INACTIVE',
+        'BLOCKED'
+    ) DEFAULT 'ACTIVE',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP
+);
